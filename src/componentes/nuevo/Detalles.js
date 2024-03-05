399981a9de8d431a72cd2111e8f3ba6644ac637e
Diego Pavon <imprimiropen@gmail.com>
2024-03-05 20:19:12 -0300

    estilos de Detalles

--- src/componentes/nuevo/Detalles.js
+++ src/componentes/nuevo/Detalles.js
@@ -1,3 +1,5 @@
+import estilos from "./Detalles.module.css";
+
 function Detalles() {
     const opcionDeHorario = ["mañana", "siesta", "tarde"];
     const serviciosDisponibles = ["semi-permanente", "capping", "softgel", "esculpidas"];
@@ -5,47 +7,59 @@ function Detalles() {
     const medioDePago = ["efectivo", "transferencia"];
 
     return (
-        <div>
-            <form>
-                <labe>
+        <div className="tarjeta">
+            <form className="p-4">
+                <label className="label">
                     Describe tu pedido de servicio
-                    <input placeholder="ej. Remocion de servicio/Nuevo servicio" />
-                </labe>
-                <label>
-                    Elige una fecha disponible <span>(15/03/2024)</span>
-                    <div>
-                        <input type="date" />
-                        <select>
+                    <input 
+                    className="input"
+                    placeholder="ej. Remocion de servicio/Nuevo servicio" />
+                </label>
+                <label className="label">
+                    Elige una fecha disponible <span>(ej. 15/03/2024)</span>
+                    <div className="flex mb-6">
+                        <input 
+                        className="input mr-6"
+                        type="date" />
+                        <select className="select">
                             {opcionDeHorario.map(opcion => <option value={opcion}>{opcion}</option>)}
                         </select>
                     </div>
                 </label>
-                <label>
+                <label className="label">
                     Elige tu servicio <span>ej. semi-permanente</span>
-                    <select>
+                    <div>
+                    <select className="select">
                         {serviciosDisponibles.map(opcion => <option value={opcion}>{opcion}</option>)}
                     </select>
+                    </div>
                 </label>
-                <label>
+                <label className="label">
                     Elige una fecha alternativa
-                    <input type="date" />
+                    <input 
+                    className="input"
+                    type="date" />
                 </label>
-                <label>
-                    Elige manos, pies o ambos <span>manos 💅/ pies 🦶/ ambos ✍️</span>
-                    <select>
+                <label className="label">
+                    Elige manos, pies o ambos <span>manos 💅/ pies 🦶/ ambos ✍️ </span>
+                    <div>
+                    <select className="select">
                         {iconos.map(opcion => <option value={opcion}>{opcion}</option>)}
                     </select>
+                    </div>
                 </label>
-                <label>
+                <label className="label">
                     Seleccione el metodo de pago
-                    <select>
+                    <div>
+                    <select className="select">
                         {medioDePago.map(opcion => <option value={opcion}>{opcion}</option>)}
                     </select>
+                    </div>
                 </label>
             </form>
-            <div>
-                <button>Crear</button>
-                <button>Cancelar</button>
+            <div className={estilos.botones}>
+                <button className="boton boton--negro">Crear</button>
+                <button className="boton boton--gris">Cancelar</button>
             </div>
         </div>
     );
